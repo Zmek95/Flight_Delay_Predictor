b3d57800592724eb7b3b82e3bd9f3521b52ade92
jandus <jalejandro.adu@gmail.com>
2021-06-27 09:26:24 -0400

    Tasks 8 -10 and fixed issue on initial load

--- src/initial_load.sql
+++ src/initial_load.sql
@@ -1,10 +1,10 @@
 
 
 -- Flights
-\copy (SELECT * FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1)) To '../data/flights_10k.csv' With CSV HEADER
+\copy (SELECT * FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1) WHERE dep_time is not null and cancelled = 0) To '../data/flights_10k.csv' With CSV HEADER
 
 -- Passengers
-\copy (SELECT * FROM passengers WHERE ROW(unique_carrier, year, month) in (SELECT mkt_unique_carrier AS unique_carrier,  date_part('year', TO_DATE(fl_date,'YYYY-MM-DD')) as year, date_part('month', TO_DATE(fl_date,'YYYY-MM-DD')) as month FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1))) To '../data//passengers_10k.csv' With CSV HEADER
+\copy (SELECT * FROM passengers WHERE ROW(unique_carrier, origin_airport_id, dest_airport_id, year, month) in (SELECT mkt_unique_carrier AS unique_carrier, origin_airport_id, dest_airport_id, date_part('year', TO_DATE(fl_date,'YYYY-MM-DD')) as year,date_part('month', TO_DATE(fl_date,'YYYY-MM-DD')) as month FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1))) To '../data//passengers_10k.csv' With CSV HEADER
 
 -- Fuel Comsumption
 \copy (SELECT * FROM fuel_comsumption WHERE ROW(unique_carrier, year, month) in (SELECT mkt_unique_carrier AS unique_carrier,  date_part('year', TO_DATE(fl_date,'YYYY-MM-DD')) as year, date_part('month', TO_DATE(fl_date,'YYYY-MM-DD')) as month FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1))) To '../data/fuel_comsumption_10k.csv' With CSV HEADER
