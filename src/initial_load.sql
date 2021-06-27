19c26a787e13a8630fd07645d1509674b05215f5
jandus <jalejandro.adu@gmail.com>
2021-06-27 10:46:03 -0400

    Initial load modification - Revmoe cancellation fillter

--- src/initial_load.sql
+++ src/initial_load.sql
@@ -1,7 +1,7 @@
 
 
 -- Flights
-\copy (SELECT * FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1) WHERE dep_time is not null and cancelled = 0) To '../data/flights_10k.csv' With CSV HEADER
+\copy (SELECT * , date_part('year', TO_DATE(fl_date,'YYYY-MM-DD')) as year, date_part('month', TO_DATE(fl_date,'YYYY-MM-DD')) as month, date_part('day', TO_DATE(fl_date,'YYYY-MM-DD')) as day FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1)) To '../data/flights_10k.csv' With CSV HEADER
 
 -- Passengers
 \copy (SELECT * FROM passengers WHERE ROW(unique_carrier, origin_airport_id, dest_airport_id, year, month) in (SELECT mkt_unique_carrier AS unique_carrier, origin_airport_id, dest_airport_id, date_part('year', TO_DATE(fl_date,'YYYY-MM-DD')) as year,date_part('month', TO_DATE(fl_date,'YYYY-MM-DD')) as month FROM flights TABLESAMPLE SYSTEM (.06153) REPEATABLE (1))) To '../data//passengers_10k.csv' With CSV HEADER
